c52ab104f590a0db026edec0689262d3c184ea30
Matt Valerio <matt.valerio@gmail.com>
2012-11-27 15:23:47 -0800

    Adding the Luhn algorithm sandbox

---
+++ Luhn/Luhn/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Luhn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool x = IsValid("7992739871");
+
+        }
+
+        public static bool IsValid(string cardNumber)
+        {
+            Func<char, int> parseChar = c => Int32.Parse(c.ToString());
+            var sum = cardNumber
+                .Select(parseChar)
+                .Select((d,i) => (i % 2 == 0) ? d : 2*d)
+                .SelectMany(d => d.ToString())
+                .Select(parseChar)
+                .Sum()
+            ;
+
+            return (sum % 10 == 0);
+        }
+
+    }
+
+    
+
+}
